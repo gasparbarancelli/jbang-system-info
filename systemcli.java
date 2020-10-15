6489bd3248600dddb8b3485170f6b7b3c331d54a
gaspar@viasoft.com.br <G45p4r19121987>
2020-10-15 15:26:01 -0300

    add uptime

--- systemcli.java
+++ systemcli.java
@@ -8,12 +8,20 @@ import picocli.CommandLine.Option;
 import com.sun.management.OperatingSystemMXBean;
 import java.io.File;
 import java.lang.management.ManagementFactory;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Callable;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.lang.reflect.Method;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Command(name = "systemcli", mixinStandardHelpOptions = true, version = "systemcli 1.0",
         description = "systemcli made with jbang")
@@ -156,6 +164,49 @@ class systemcli implements Callable<Integer> {
 
         Long discoEmGb = total / 1024 / 1024 / 1000;
         print("Espaco total de disco", longToStr(discoEmGb) + "GB");
+    }
+
+    @Option(names = { "-u", "--uptime" }, description = "Exibe há quanto tempo seu sistema está em atividade")
+    public void showUptime(boolean show) throws Exception {
+        long uptime = -1;
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            Process uptimeProc = Runtime.getRuntime().exec("wmic os get lastbootuptime");
+            BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream(), StandardCharsets.UTF_8));
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (line.startsWith("LastBootUpTime") || line.trim().equals("")) {
+                    continue;
+                }
+                String lastBootUpTime = line.split("\\.")[0];
+                SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+                Date bootTime = format.parse(lastBootUpTime);
+                uptime = System.currentTimeMillis() - bootTime.getTime();
+                break;
+            }
+        } else if (os.contains("mac") || os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            Process uptimeProc = Runtime.getRuntime().exec("uptime");
+            BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
+            String line = in.readLine();
+            if (line != null) {
+                Pattern parse = Pattern.compile("((\\d+) days,)? (\\d+):(\\d+)");
+                Matcher matcher = parse.matcher(line);
+                if (matcher.find()) {
+                    String _days = matcher.group(2);
+                    String _hours = matcher.group(3);
+                    String _minutes = matcher.group(4);
+                    int days = _days != null ? Integer.parseInt(_days) : 0;
+                    int hours = _hours != null ? Integer.parseInt(_hours) : 0;
+                    int minutes = _minutes != null ? Integer.parseInt(_minutes) : 0;
+                    uptime = TimeUnit.MILLISECONDS.convert(days, TimeUnit.DAYS) +
+                            TimeUnit.MILLISECONDS.convert(hours, TimeUnit.HOURS) +
+                            TimeUnit.MILLISECONDS.convert(minutes, TimeUnit.MINUTES);
+                }
+            }
+        }
+
+        print("Tempo de atividade (horas): ", "" + TimeUnit.HOURS.convert(uptime, TimeUnit.MILLISECONDS));
+        print("Tempo de atividade (dias): ", "" + TimeUnit.DAYS.convert(uptime, TimeUnit.MILLISECONDS));
     }
 
     private String longToStr(Long value) {
